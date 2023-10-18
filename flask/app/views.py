c6794fe71901d124c29e441d50b756322855c9e6
vincent-laizer <laizercorp@gmail.com>
2023-10-18 10:29:13 +0300

    check-in and assessment pages and routes

--- flask/app/views.py
+++ flask/app/views.py
@@ -5,3 +5,11 @@ from flask import render_template
 @app.route('/home')
 def index():
     return render_template('index.html', title='Welcome')
+
+@app.route('/check-in')
+def check_in():
+    return render_template('checkin.html', title='Early Check-In')
+
+@app.route('/assessment')
+def assessment():
+    return render_template('assessment.html', title='Assessment')
