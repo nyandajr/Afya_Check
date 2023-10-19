91a121eeaac164de7db28f0f5d783bb29de54efe
vincent-laizer <laizercorp@gmail.com>
2023-10-19 09:33:17 +0300

    fetch check-in opts from db

--- flask/app/views.py
+++ flask/app/views.py
@@ -3,7 +3,7 @@ from flask import render_template, flash, request, redirect, url_for
 import plotly.graph_objs as go
 from plotly.graph_objects import Layout
 from flask_login import login_user, logout_user, current_user, login_required
-from app.schema import User, CheckIn
+from app.schema import User, CheckIn, CheckInOption
 
 @app.route('/')
 @app.route('/home')
@@ -12,105 +12,7 @@ def index():
 
 @app.route('/check-in')
 def check_in():
-    items = [
-        {
-            "id": 1,
-            "title": "Consistence fear or worries",
-            "options": [
-                {
-                    "id": 1,
-                    "text": "Often feeling overwhelmed with irrational fears, avoiding certain places \
-                        due to worries, and frequently feeling scared without a clear reason"
-                },
-                {
-                    "id": 2,
-                    "text": "struggles with the fear of experiencing panic attacks"
-                },
-                {
-                    "id": 3,
-                    "text": "displays visible trembling or shaking without physical exertion"
-                },
-                {
-                    "id": 4,
-                    "text": "struggles with fears of being judged for expressing emotions"
-                }
-            ]
-        },
-        {
-            "id": 2,
-            "title": "Experience of unreal perceptions",
-            "options": [
-                {
-                    "id": 1,
-                    "text": "Often hearing voices others can't, being suspicious of others, and \
-                        feeling that others can read your thoughts"
-                },
-                {
-                    "id": 2,
-                    "text": "exhibits signs of magical thinking, believing in supernatural forces \
-                        affecting their lives"
-                },
-                {
-                    "id": 3,
-                    "text": "struggles to distinguish between hallucinations and reality"
-                }
-            ]
-        },
-        {
-            "id": 3,
-            "title": "Challenges with memory and cognition",
-            "options": [
-                {
-                    "id": 1,
-                    "text": "Challenged in reasoning and problem solving, forgetting names, and frequently unaware of the current date or time"
-                }
-            ]
-        },
-        {
-            "id": 4,
-            "title": "Persistent sadness or hopelessness",
-            "options": [
-                {
-                    "id": 1,
-                    "text": "Often contemplating death, consistently feeling anxious, and experiencing \
-                         disruptions in appetite"
-                },
-                {
-                    "id": 2,
-                    "text": "suffers from disrupted sleep patterns and early morning awakenings"
-                },
-                {
-                    "id": 3,
-                    "text": "experiences persistent thoughts of death and suicide"
-                },
-                {
-                    "id": 4,
-                    "text": "shows signs of neglecting personal hygiene and appearance"
-                }
-            ]
-        },
-        {
-            "id": 5,
-            "title": "Reliance on substance use",
-            "options": [
-                {
-                    "id": 1,
-                    "text": "Difficult to manage tasks without drugs or alcohol, prioritizing drug use, and feeling a constant need to use substances"
-                }
-            ]
-        },
-        {
-            "id": 6,
-            "title": "Extreme Mood Swings",
-            "options": [
-                {
-                    "id": 1,
-                    "text": "Experiencing overly happy and sad periods, being very active or talkative, and frequently engaging in risky activities"
-                }
-            ]
-        }
-    ]
-    
+    items = []
     ci = CheckIn.query.all()
     for c in ci:
         items.append(c.to_dict())
