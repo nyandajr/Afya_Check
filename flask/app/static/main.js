6a927cd1d5baa7194cd6d1819eaad5280417438f
Nyanda Jr <freddynyanda@proton.me>
2023-12-21 12:11:09 +0300

    Added the spinning functionality in the JS for checkin button and also improved its style in css. Awesome

--- flask/app/static/main.js
+++ flask/app/static/main.js
@@ -1,64 +1,74 @@
 $(document).ready(function() {
-    // hide results and alert divs
-    $("#results").hide();
-    $("#alert").hide();
-
-    var lang = $("#checkInContainer").data('lang'); // Get the current language
-
-    // Messages for different languages
+    // Get the current language
+    var lang = $("#checkInContainer").data('lang');
+  
+    // Define message objects for different languages
     var messages = {
-        en: {
-            symptomMessage: 'Based on your symptoms, you might be showing signs of ',
-            recommendation: 'We recommend taking the following assessments for a more comprehensive evaluation.'
-        },
-        sw: {
-            symptomMessage: 'Kulingana na dalili zako, unaweza kuwa unaonyesha dalili za ',
-            recommendation: 'Tunapendekeza ufanye tathmini zifuatazo kwa tathmini kamili zaidi.'
-        }
+      en: {
+        symptomMessage: 'Based on your symptoms, you might be showing signs of ',
+        recommendation: 'We recommend taking the following assessments for a more comprehensive evaluation.',
+      },
+      sw: {
+        symptomMessage: 'Kulingana na dalili zako, unaweza kuwa unaonyesha dalili za ',
+        recommendation: 'Tunapendekeza ufanye tathmini zifuatazo kwa tathmini kamili zaidi.',
+      },
     };
-
+  
+    // Handle form submission
     $("#checkInForm").submit((e) => {
-        e.preventDefault();
-        $("#checkButton").html(lang === "sw" ? "Inatathmini..." : "Checking...");
-
-        $.ajax({
-            url: '', // Update with your endpoint
-            type: 'POST',
-            data: $("#checkInForm").serialize(),
-            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-            success: function(response) {
-                if (response.error) {
-                    $("#alert").removeClass("alert-success").addClass("alert-danger").text(response.error).show();
-                    $("#results").hide();
-                } else {
-                    $("#alert").hide();
-
-                    const predictedCondition = response.predicted_condition;
-                    const recommendedAssessments = response.recommended_assessments;
-
-                    let resultsMessage = `${messages[lang].symptomMessage}<strong>${predictedCondition}</strong>. ${messages[lang].recommendation}`;
-                    $("#resultsContent").html(resultsMessage); // Dynamically set the message
-                    
-                    $("#resultsLink").empty(); // Clear previous content
-                    recommendedAssessments.forEach(assessment => {
-                        $("#resultsLink").append(`<a href='/assessment/${assessment}' class='link'>${assessment}</a><br>`);
-                    });
-
-                    $("#results").show(); // Show the results div
-
-                    // scroll to page bottom
-                    $('html, body').animate({
-                        scrollTop: $(document).height()
-                    }, 500);
-                }
-            },
-            error: function (error) {
-                $("#alert").removeClass("alert-success").addClass("alert-danger").text("Something went wrong. Please try again later.").show();
-            },
-            complete: function() {
-                $("#checkButton").html(lang === "sw" ? "Tathmini" : "Check In");
-            }
-        });  
+      e.preventDefault();
+  
+      // Add spinner and text with language-specific message
+      $("#checkButton").html(`<span class="spinner-border spinner-border-sm"></span> ${lang === "sw" ? "Inatathmini..." : "Checking..."}`);
+      $(".spinner-border").addClass("animate-spin"); // Animate the spinner
+  
+      // Send AJAX request
+      $.ajax({
+        url: '', // Replace with your actual endpoint URL
+        type: 'POST',
+        data: $("#checkInForm").serialize(),
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        success: function(response) {
+          // Handle successful response
+          if (response.error) {
+            // Show error message and hide results
+            $("#alert").removeClass("alert-success").addClass("alert-danger").text(response.error).show();
+            $("#results").hide();
+          } else {
+            // Hide alert and handle response data
+            $("#alert").hide();
+  
+            const predictedCondition = response.predicted_condition;
+            const recommendedAssessments = response.recommended_assessments;
+  
+            // Build and display results message with translation
+            let resultsMessage = `${messages[lang].symptomMessage}<strong>${predictedCondition}</strong>. ${messages[lang].recommendation}`;
+            $("#resultsContent").html(resultsMessage);
+  
+            // Clear and populate recommended assessments list
+            $("#resultsLink").empty();
+            recommendedAssessments.forEach(assessment => {
+              $("#resultsLink").append(`<a href='/assessment/${assessment}' class='link'>${assessment}</a><br>`);
+            });
+  
+            // Show results and scroll to bottom
+            $("#results").show();
+            $('html, body').animate({
+              scrollTop: $(document).height()
+            }, 500);
+          }
+  
+          // Remove spinner and restore button text regardless of success or error
+          $(".spinner-border").removeClass("animate-spin");
+          $("#checkButton").text(lang === "sw" ? "Tathmini imekamilika" : "Check  Complete");
+        },
+        error: function (error) {
+          // Handle AJAX errors
+          $("#alert").removeClass("alert-success").addClass("alert-danger").text("Something went wrong. Please try again later.").show();
+          $(".spinner-border").removeClass("animate-spin");
+          $("#checkButton").text(lang === "sw" ? "Kagua tena" : "Check In");
+        },
+      });  
     });
 });
 
