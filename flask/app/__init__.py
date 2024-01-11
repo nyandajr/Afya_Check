efdc75b87406df9d23cb6a8f2f18a0a1e08c361c
Freddy Nyanda <freddynyanda@proton.me>
2024-01-11 20:43:01 +0300

    re-modified the init.py file

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -7,9 +7,8 @@ from flask_bcrypt import Bcrypt  # Import Bcrypt
 
 app = Flask(__name__)
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///afya_check.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = '''postgres://lajtmhynnlgztg:bedc78ff8cf870eba8f875debf44c60d54b30c9f409737c2032c4608dc3349a1@ec2-35-169-9-79.compute-1.amazonaws.com:5432/dasgr4ki63k3je
-'''
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///afya_check.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///afya_check.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = "secret_key_from_env"
 
